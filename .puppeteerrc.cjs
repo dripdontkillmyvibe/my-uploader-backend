bcc286c4c17b4403c501a71a64d953b4931a7bdc
dripdontkillmyvibe <132927699+dripdontkillmyvibe@users.noreply.github.com>
2025-07-10 09:43:56 -0400

    Update .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -4,7 +4,6 @@ const { join } = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // This is the important part.
-  // It tells Puppeteer to download the browser to a local, predictable folder.
+  // This tells Puppeteer to download the browser to a local, predictable folder.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
